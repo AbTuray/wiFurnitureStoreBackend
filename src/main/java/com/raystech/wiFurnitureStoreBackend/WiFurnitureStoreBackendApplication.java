315f7409b6a7d33b34721cb62fe80a1e4d622c83
AbTuray <turayabdulai570@gmail.com>
2026-02-04 14:40:37 +0000

    Initializing git repo

---
+++ src/main/java/com/raystech/wiFurnitureStoreBackend/WiFurnitureStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.raystech.wiFurnitureStoreBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiFurnitureStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiFurnitureStoreBackendApplication.class, args);
+	}
+
+}
